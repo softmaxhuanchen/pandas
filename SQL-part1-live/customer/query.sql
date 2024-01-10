4aaff7d5624bbc2738bae9721c32914e6d3be111
Huanchen Song <softmaxmcsearch@gmail.com>
2024-01-09 19:26:51 -0500

    update

--- SQL-part1-live/customer/query.sql
+++ SQL-part1-live/customer/query.sql
@@ -1,6 +1,15 @@
 -- Query only the `first_name` field
-
+SELECT first_name
+FROM customer;
 
 -- Filter the query to show only male customers under the age of 34
+SELECT first_name, last_name
+FROM customer
+WHERE gender = 'Male'
+AND age < 34;
 
 -- Alias
+SELECT c.first_name, c.last_name
+FROM customer as c
+WHERE c.gender = 'Male'
+AND c.age < 34;
