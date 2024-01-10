4543cfdc1a04b7553af70693dbba2278c9d9b7cc
Huanchen Song <softmaxmcsearch@gmail.com>
2024-01-09 21:32:17 -0500

    update

--- SQL-part1-live/mortage/query.sql
+++ SQL-part1-live/mortage/query.sql
@@ -36,27 +36,34 @@ FULL OUTER JOIN mortgage AS m ON s.mortgage_id = m.mortgage_id;
 -- Query challenges 
 
 -- 1. List All Mortgages: Retrieve all columns from the mortgage table.
-
+SELECT * FROM mortgage;
 
 -- 2. Total Number of Sales: Count the total number of sales records in the sales table.
 
-
+SELECT COUNT(*) FROM sales;
 
 -- 3. Find Specific Loan Amounts: Select all records from the sales table where the loan_amount is greater than 300,000.
 
-
+SELECT * FROM sales
+WHERE loan_amount > 300000;
 
 -- Sum of Loans for Each Mortgage Type: Calculate the total sum of loan_amount for each mortgage_id.
 
-
+SELECT mortgage_id, SUM(loan_amount) AS total_loan_amount
+FROM sales
+GROUP BY mortgage_id
 
 --Average Loan Amount per Mortgage Type: Calculate the average loan amount for each type of mortgage.
 
-
+SELECT mortgage_id, AVG(loan_amount) AS AVG_loan_amount
+FROM sales
+GROUP BY mortgage_id
 
 -- Join Mortgages and Sales: List all sales along with the corresponding mortgage name and rate.
 
-
+SELECT s.*, m.mortgage_name, m.mortgage_rate
+FROM sales 	AS s
+JOIN mortgage AS m ON s.mortgage_id = m.mortgage_id;
 
 -- Mortgages with Highest Loan Amounts: Find the mortgage names with the top three highest total loan amounts.
 
